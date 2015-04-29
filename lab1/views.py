60bc7ce08442fb699c178873348f651abd4dd892
SamsonicX <samsonicx@mail.ru>
2015-04-30 03:14:15 +0600

    Второй Коммит
    Я пытался, но кое-что не заладилось. Допишу завтра. Основная проблема:
    что-то с сервером и, соответственно, со структурой классов в программе.

--- lab1/views.py
+++ lab1/views.py
@@ -6,37 +6,58 @@ class IndexView(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
-        Subjects = [Subject(1, "timp")]
-        Subject(2, "eis")
-        Subject(3, "philosophy")
-        Subject(4, "english")
-        Subject(5, "sport")
-
-
-        list_students = [
-            Student(1, "surname1", "name1", "patronymic1", "group1", 20),
-            Student(2, "surname2", "name2", "patronymic2", "group2", 20),
-            Student(3, "surname3", "name3", "patronymic3", "group2", 20),
-            Student(4, "surname4", "name4", "patronymic4", "group1", 20),
-            Student(5, "surname5", "name5", "patronymic5", "group1", 20),
-            Student(6, "surname6", "name6", "patronymic6", "group3", 20),
-            Student(7, "surname7", "name7", "patronymic7", "group3", 20),
-            Student(8, "surname8", "name8", "patronymic8", "group2", 20),
-            Student(9, "surname9", "name9", "patronymic9", "group1", 20),
-            Student(10, "surname10", "name10", "patronymic10", "group1", 20),
+        subjects = [
+            Subject("timp"),
+            Subject("eis"),
+            Subject("philosophy"),
+            Subject("english"),
+            Subject("sport")
         ]
 
-        List_Score = Score()
-        List_Score.add_fixed(1, 4, 5, 3, 4, 2)
-        List_Score.add_fixed(2, 4, 5, 5, 5, 4)
-        List_Score.add_fixed(3, 4, 5, 5, 5, 4)
-        List_Score.add_fixed(4, 2, 3, 5, 5, 4)
-        List_Score.add_fixed(5, 2, 2, 3, 3, 2)
-        List_Score.add_fixed(6, 4, 5, 2, 5, 4)
-        List_Score.add_fixed(7, 4, 5, 5, 5, 4)
-        List_Score.add_fixed(8, 4, 5, 5, 3, 4)
-        List_Score.add_fixed(9, 2, 5, 5, 5, 4)
-        List_Score.add_fixed(10, 2, 1, 1, 1, 4)
+        list_students = [
+            Student(0, "surname1", "name1", "patronymic1", "group1", 20),
+            Student(1, "surname2", "name2", "patronymic2", "group2", 20),
+            Student(2, "surname3", "name3", "patronymic3", "group2", 20),
+            Student(3, "surname4", "name4", "patronymic4", "group1", 20),
+            Student(4, "surname5", "name5", "patronymic5", "group1", 20),
+            Student(5, "surname6", "name6", "patronymic6", "group3", 20),
+            Student(6, "surname7", "name7", "patronymic7", "group3", 20),
+            Student(7, "surname8", "name8", "patronymic8", "group2", 20),
+            Student(8, "surname9", "name9", "patronymic9", "group1", 20),
+            Student(9, "surname10", "name10", "patronymic10", "group1", 20),
+        ]
+
+        list_score = Score()
+        list_score.add_fixed(0, 4, 5, 3, 4, 2)
+        list_score.add_fixed(1, 4, 5, 5, 5, 4)
+        list_score.add_fixed(2, 4, 5, 5, 5, 4)
+        list_score.add_fixed(3, 2, 3, 5, 5, 4)
+        list_score.add_fixed(4, 2, 2, 3, 3, 2)
+        list_score.add_fixed(5, 4, 5, 2, 5, 4)
+        list_score.add_fixed(6, 4, 5, 5, 5, 4)
+        list_score.add_fixed(7, 4, 5, 5, 3, 4)
+        list_score.add_fixed(8, 2, 5, 5, 5, 4)
+        list_score.add_fixed(9, 2, 1, 1, 1, 4)
+
+        list_average = []
+        for student_id in range(len(list_students)):
+            counter = 0
+            for subject_id in range(len(subjects)):
+                counter += list_score(student_id * len(subjects) + subject_id).value
+            list_average.append(counter / len(subjects))
+
+        students_statistics = []
+        for student in len(list_students):
+            student_info = {
+                'id': student_id + 1,
+                'fio': student.familia + " " + student.imya + " " + student.otchestvo,
+                'timp': list_score(student_id * len(subjects)).value,
+                'eis': list_score(student_id * len(subjects) + 1).value,
+                'philosophy': list_score(student_id * len(subjects) + 2).value,
+                'sport': list_score(student_id * len(subjects) + 3).value,
+                'average': list_average(student_id)
+            }
+            students_statistics.append(student_info)
 
         context = super(IndexView, self).get_context_data(**kwargs)
         context.update(
@@ -67,30 +88,23 @@ class Person:
         self.otchestvo = otchestvo
 
 class Student(Person):
-    def __init__(self, id, familia, imya, otchestvo, group, age):
-        self.id = id
+    def __init__(self, student_id, familia, imya, otchestvo, group, age):
+        self.student_id = student_id
         self.person = Person(familia, imya, otchestvo)
         self.group = group
         self.age = age
 
-    def show(self):
-        print self.id, self.group, self.age
 
 class Subject:
-    list = []
-    def __init__(self, id, name):
-        self.list.append((id, name))
+    def __init__(self, name):
+        self.list.append(name)
 
-    def show(self):
-        print self.id, self.name
 
 class Score:
     table = []
-    def __init__(self):
-        pass
 
-    def add(self, st_id, su_id, value):
-        self.table.append((st_id, su_id, value))
+    def __init__ (self, st_id, sb_id, value):
+        self.table.append((st_id, sb_id, value))
 
     def add_fixed(self, st_id, value1, value2, value3, value4, value5):
         self.table.append((st_id, 1, value1))
